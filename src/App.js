c7bb0f0b4d67f0eae496ee6663fa1f6d1c6a2645
wojtek <wojtekpietraszuk@gmail.com>
2022-05-05 09:10:17 +0200

    Add title, basic game stats and Grid component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import Modal from './components/Modal/Modal';
+import Grid from './components/Grid/Grid';
 
 function App() {
 	const [show, setShow] = useState(true);
@@ -9,7 +10,17 @@ function App() {
 		setShow(false);
 	};
 
-	return <>{show && <Modal onShow={hideModal} />}</>;
+	return (
+		<div className='App'>
+			<h1>Memory Game!</h1>
+			<div className='stats'>
+				<p>Steps: 0</p>
+				<p>Time: 00:00</p>
+			</div>
+			{show && <Modal onShow={hideModal} />}
+			<Grid />
+		</div>
+	);
 }
 
 export default App;
