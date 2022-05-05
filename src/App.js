c4e96c7caa63d848cdb9373206a5013502433207
wojtek <wojtekpietraszuk@gmail.com>
2022-05-05 10:57:04 +0200

    Implement steps counter

--- src/App.js
+++ src/App.js
@@ -4,25 +4,26 @@ import Modal from './components/Modal/Modal';
 import Grid from './components/Grid/Grid';
 
 function App(props) {
+	const [steps, setSteps] = useState(0);
 	const [show, setShow] = useState(true);
 
 	const hideModal = () => {
 		setShow(false);
 	};
 
-	const setps = (input) => {
-		console.log(input)
+	const changeStepsHandler = (input) => {
+		setSteps(input);
 	}
 
 	return (
 		<div className='App'>
 			<h1>Memory Game!</h1>
 			<div className='stats'>
-				<p>Steps: 0</p>
+				<p>Steps: {steps}</p>
 				<p>Time: 00:00</p>
 			</div>
 			{show && <Modal onShow={hideModal} />}
-			<Grid onChangeSteps={setps}/>
+			<Grid onChangeSteps={changeStepsHandler}/>
 		</div>
 	);
 }
