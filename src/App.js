6f6a0f891f6d32a281a368effe6aba7136ae1157
wojtek <wojtekpietraszuk@gmail.com>
2022-05-06 16:45:21 +0200

    Implement timer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import './App.css';
 import Modal from './components/Modal/Modal';
 import Grid from './components/Grid/Grid';
 
-function App(props) {
+const App = () => {
 	const [steps, setSteps] = useState(0);
 	const [show, setShow] = useState(true);
+	const [inter, setInter] = useState(null);
+
+	const [seconds, setSeconds] = useState(0);
+	const [minutes, setMinutes] = useState(0);
+	
+	if (seconds === 60) {
+		setMinutes((prevState) => prevState + 1);
+		setSeconds(0);
+	}
+
+	if (minutes === 60) {
+		setMinutes(0);
+	}
 
 	const hideModal = () => {
 		setShow(false);
@@ -15,12 +28,31 @@ function App(props) {
 		setSteps(stepsData);
 	};
 
+	const timer = useCallback(() => {
+		if (show === true) {
+			return;
+		}
+		
+		setSeconds((prevState) => prevState + 1);
+	}, [show]);
+
+	useEffect(() => {
+		if (show === false && inter === null) {
+			setInter(setInterval(timer, 1000));
+		}
+
+		if (show === true) {
+			clearInterval(inter);
+			setInter(null);
+		}
+	}, [show, timer, inter]);
+
 	return (
 		<div className='App'>
 			<h1>Memory Game!</h1>
 			<div className='stats'>
 				<p>Steps: {steps}</p>
-				<p>Time: 00:00</p>
+				<p>Time: {`${minutes < 10 ? `0${minutes}` : `${minutes}`}:${seconds < 10 ? `0${seconds}` : `${seconds}`}`}</p>
 			</div>
 			{show && <Modal onShow={hideModal} />}
 			<Grid onChangeSteps={changeStepsHandler} />
