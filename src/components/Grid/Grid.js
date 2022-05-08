10dc9739a33c0f7d8ec27a6bc33473858aaefb6b
wojtek <wojtekpietraszuk@gmail.com>
2022-05-08 16:14:29 +0200

    Add logic to show color of clicked card

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -1,48 +1,111 @@
-import { useContext } from 'react';
+import { useContext, Fragment, useState, useEffect } from 'react';
 import AuthContext from '../../store/steps-context';
 import classes from './Grid.module.css';
 
-
 const colors = [
-	'red',
-	'yellow',
-	'green',
-	'blue',
-	'purple',
-	'pink',
-	'white',
-	'grey',
-	'brown',
-	'orange',
+	{
+		color: 'red',
+		id: 'color-1',
+	},
+	{
+		color: 'red',
+		id: 'color-2',
+	},
+	{
+		color: 'yellow',
+		id: 'color-3',
+	},
+	{
+		color: 'yellow',
+		id: 'color-4',
+	},
+	{
+		color: 'green',
+		id: 'color-5',
+	},
+	{
+		color: 'green',
+		id: 'color-6',
+	},
+	{
+		color: 'blue',
+		id: 'color-7',
+	},
+	{
+		color: 'blue',
+		id: 'color-8',
+	},
+	{
+		color: 'purple',
+		id: 'color-9',
+	},
+	{
+		color: 'purple',
+		id: 'color-10',
+	},
+	{
+		color: 'pink',
+		id: 'color-11',
+	},
+	{
+		color: 'pink',
+		id: 'color-12',
+	},
+	{
+		color: 'grey',
+		id: 'color-13',
+	},
+	{
+		color: 'grey',
+		id: 'color-14',
+	},
+	{
+		color: 'brown',
+		id: 'color-15',
+	},
+	{
+		color: 'brown',
+		id: 'color-16',
+	},
+	{
+		color: 'orange',
+		id: 'color-17',
+	},
+	{
+		color: 'orange',
+		id: 'color-18',
+	},
 ];
 
+const shuffledColors = colors.sort(() => 0.5 - Math.random());
+
 const Grid = () => {
+	const [isSelected, setIsSelected] = useState('');
 	const authCtx = useContext(AuthContext);
 
-	const clicker = (event) => {
-		const element = event.target.classList[0];
+	const clickHandler = (event) => {
+		const element = event.target;
 		authCtx.stepCounter(element);
-	}
+	};
+
+	const handleClick = (id) => {
+		setIsSelected(id);
+	};
 
 	return (
-		<div
-			className={classes.grid}
-			onClick={clicker}
-		>
-			<div className='card'></div>
-			<div className='card'></div>
-			<div className='card'></div>
-			<div className='card'></div>
-			<div className='card'></div>
-			<div className='card'></div>
-			<div className='card'></div>
-			<div className='card'></div>
-			<div className='card'></div>
-			<div className='card'></div>
-			<div className='card'></div>
-			<div className='card'></div>
-			<div className='card'></div>
-			<div className='card'></div>
+		<div className={classes.grid} onClick={clickHandler}>
+			{shuffledColors.map((item) => (
+				<Fragment key={item.id}>
+					<div
+						className={
+							isSelected === item.id
+								? item.color + ' card'
+								: item.color + ' hide card'
+						}
+						onClick={() => handleClick(item.id)}
+					/>
+				</Fragment>
+			))}
 		</div>
 	);
 };
