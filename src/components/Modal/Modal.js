b37662a68ff3a9aadaee70ddfe2b07a96f2c3603
wojtek <wojtekpietraszuk@gmail.com>
2022-05-13 17:25:05 +0200

    Create portals

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,23 +1,35 @@
-import { useState } from 'react';
+import { Fragment, useContext } from 'react';
+import { createPortal } from 'react-dom';
 import classes from './Modal.module.css';
+import StepContext from '../../store/steps-context';
 
 const Modal = (props) => {
-	const [isFinished, setIsFinished] = useState(false);
+	const stepCtx = useContext(StepContext);
+
+	const resetGame = () => {
+		props.onEndGame()
+		stepCtx.resetStepCounter()
+	}
 
 	return (
-		<div className={classes.modal}>
-			<h2>Memory Game!</h2>
-			{isFinished && (
-				<>
-					<p>Time: 00:00</p>
-					<p>Steps: 0</p>
-				</>
+		<Fragment>
+			{createPortal(
+				<div className={classes.modal}>
+					<h2>Memory Game!</h2>
+					{props.onEndGame && (
+						<>
+							<p>Time: 00:00</p>
+							<p>Steps: {stepCtx.steps}</p>
+						</>
+					)}
+					{!props.onEndGame && <p>Start the game</p>}
+					<button onClick={resetGame}>
+						Play!
+					</button>
+				</div>,
+				document.getElementById('modal')
 			)}
-			{!isFinished && <p>Start the game</p>}
-			<button onClick={props.onShow} onChange={props.onClick}>
-				Play!
-			</button>
-		</div>
+		</Fragment>
 	);
 };
 
